84eaf021ffccd8e00c33b4ad2b54ed04f107093d
unknown <christianjhegggfer@gmail.com>
2023-03-29 22:13:10 +0800

    added conditional routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,51 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {
   RouterProvider,
   Route,
   createRoutesFromElements,
   createBrowserRouter,
 } from "react-router-dom";
-import {ToastContainer} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import MainLayout from "./components/MainLayout";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import LandingPage from "./pages/LandingPage";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<LandingPage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register/>} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Route>
-  )
-);
+import NotFound from "./pages/Notfound";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const { user } = useSelector((state) => state.auth);
+  const isAuthenticated = Boolean(user);
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        {isAuthenticated ? 
+        (
+          <>
+            <Route index element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </>
+        ) : (
+          <>
+            <Route index element={<LandingPage />} id="default" />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </>
+        )}
+        <Route path="*" element={<NotFound />} id="404" />
+      </Route>
+    )
+  );
+
   return (
-  <>
-    <RouterProvider router={router} />
-    <ToastContainer/>
-  </>
+    <>
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </>
   );
 };
 
---
+++ client/src/pages/Notfound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Notfound = () => {
+  return (
+    <div>
+        <p>Error: 404</p>
+        <p>URL NOT FOUND</p>
+    </div>
+  )
+}
+
+export default Notfound
